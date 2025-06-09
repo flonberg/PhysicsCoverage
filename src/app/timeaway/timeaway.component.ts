a172e56d853053e7cfc195cbb1209fe3baeffe79
flonberg <48604516+flonberg@users.noreply.github.com>
2025-06-09 16:43:31 -0400

    pass th euserid

--- src/app/timeaway/timeaway.component.ts
+++ src/app/timeaway/timeaway.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-timeaway',
   standalone: true,
@@ -9,14 +10,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TimeawayComponent {
   id: string | null = null;
+  url: SafeResourceUrl | null = null;
   
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private sanitizer:DomSanitizer) {}
  ngOnInit() {
      this.route.params.subscribe(params => {
       this.id = params['id'];
+      let url="https://whiteboard.partners.org/esb/FLwbe/vacation/vacmanWU.php?userid="+this.id+"&vidx=0&first=vM&func=0"
+      this.sanitizeURL(url);
    //     window.location.href = 'https://whiteboard.partners.org/esb/FLwbe/vacation/indexPHPsmall.php?userid=' + this.id +'&vidx=0&first=vM&func=0'
      })
   
   }
-
+  sanitizeURL(url:string) {
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
