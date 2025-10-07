b101258ad5bcecc658cc9259df5752fcb2b3ebb5
flonberg <48604516+flonberg@users.noreply.github.com>
2025-10-07 09:51:20 -0400

    AngMat DateRangePicker working

--- src/app/angtimeaway/angtimeaway.component.ts
+++ src/app/angtimeaway/angtimeaway.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule, DateAdapter } from '@angular/material/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,10 +10,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './angtimeaway.component.html',
   styleUrls: ['./angtimeaway.component.css'],
   standalone: true,
-  imports: [CommonModule,  MatDatepickerModule, MatNativeDateModule]
+  imports: [
+    CommonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
+  ],
+  providers: [MatDatepickerModule, MatNativeDateModule]
 })
 export class AngtimeawayComponent implements OnInit {
-
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl()
+  });
 
   constructor() {
 
