56722a20026535075ba6371344cf2e30d19b767c
flonberg <48604516+flonberg@users.noreply.github.com>
2025-11-17 17:18:11 -0500

    last of Nov 17 2025

--- src/app/php/editTAs.php
+++ src/app/php/editTAs.php
@@ -4,7 +4,6 @@ require_once 'H:\inetpub\lib\sqlsrvLibFL.php';
 require_once 'H:\inetpub\lib\LogFuncs.php';
 $handle = connectDB_FL();
     $log = new LogFuncs();
-
    $dstr = print_r($_GET, true); fwrite($fp, $dstr);
    $log->logMessage("Received GET parameters: ". $dstr);
    $updateSts = "UPDATE top(1) vacation3 SET ".$_GET['newValueName']." = '".$_GET['newValue']."' WHERE vidx = ".$_GET['vidx'];    $log->logSql($updateSts);
--- src/app/php/enterTriageCov.php
+++ src/app/php/enterTriageCov.php
@@ -5,22 +5,23 @@ require_once 'H:\inetpub\lib\LogFuncs.php';
 $handle = connectDB_FL();
     $log = new LogFuncs();
 
-   $log->logMessage("Script started");
+
    $now = date("Y-m-d h:i:s");
-   $dstr = print_r($_GET, true); fwrite($fp, $dstr);
+   $log->logMessage("Current time: ".$now);
+   $log->logMessage("Script started at ".$now." with parameters: ". print_r($_GET, true));
  
    if ($_GET['userkey'] == '')
    {
-      fwrite($fp, "\r\n No userkey passed \r\n");
+      $log->logMessage("No userkey passed");
       exit;    
    }
    $insStr = "INSERT INTO ResidentTriageDuty (userkey, day, modWhen) VALUES (".$_GET['userkey'].", '".$_GET['date']."', '".$now."')";
-   fwrite($fp, "\r\n $insStr");
+   $log->logMessage("Executing insert: ".$insStr);
    $stmt = sqlsrv_query( $handle, $insStr);
    if( $stmt === false ) 
-       { $dstr = ( print_r( sqlsrv_errors(), true)); fwrite($fp, "\r\n errors: \r\n ".$dstr); } 
+       { $log->logMessage("SQL errors: ". print_r( sqlsrv_errors(), true)); } 
    else
-       { fwrite($fp, "\r\n Inserted successfully \r\n"); }
+       { $log->logMessage("Inserted successfully    "); }
    sqlsrv_free_stmt( $stmt);
    sqlsrv_close( $handle);
    fclose($fp);   
